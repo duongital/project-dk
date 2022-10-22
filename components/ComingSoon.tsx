d736c7663f23df11f7da20bd0830688911667db6
Duong Nguyen <duongital@gmail.com>
2022-10-22 15:11:35 +0700

    chore: add tailwind css

---
+++ components/ComingSoon.tsx
@@ -0,0 +1,7 @@
+export default function ComingSoon() {
+  return (
+    <div className=" w-screen h-screen flex items-center justify-center border-8 border-blue-100">
+      <h1 className="text-3xl font-bold text-blue-400">Coming Soon!</h1>
+    </div>
+  );
+}

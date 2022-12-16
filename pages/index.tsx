61ca241af463ed5763e8e076cdb3197cc0219ca1
Duong Nguyen <duongital@gmail.com>
2022-12-16 19:21:01 +0700

    feat: add preview image on social

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,12 +8,17 @@ import Cover from 'components/Cover'
 import Introduction from 'components/Introduction'
 import Footer from 'components/Footer'
 
+import CDCR from 'assets/CDCR.jpg'
+
 const Home: NextPage = () => {
+  const title = 'Đại Dương & Hoàng Khánh'
+  const description = 'Cùng gửi lời chúc mừng đến chú rể & cô dâu ngay nhé!'
+
   return (
     <>
       <Head>
-        <title>Đại Dương & Hoàng Khánh</title>
-        <meta name="description" content="Cùng gửi lời chúc mừng đến chú rể & cô dâu ngay nhé!" />
+        <title>{title}</title>
+        <meta name="description" content={description} />
         <link rel="icon" href="/favicon.svg" sizes="any" type="image/svg+xml" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
@@ -21,6 +26,9 @@ const Home: NextPage = () => {
           href="https://fonts.googleapis.com/css2?family=Be+Vietnam+Pro:wght@300;400;600&family=Cedarville+Cursive&display=swap"
           rel="stylesheet"
         />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={description} />
+        <meta property="og:image" content={CDCR.src} />
       </Head>
 
       <Cover />
